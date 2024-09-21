58e8375be934cde4fb77542e4060828a9ae1d3d9
Patrick Christie <patrick.christie.dev@gmail.com>
2024-09-22 08:09:28 +1000

    Implement basic colouring of cells when mouse is pressed

--- foundations/Javascript-Basics/Etch-a-Sketch/resources/main.js
+++ foundations/Javascript-Basics/Etch-a-Sketch/resources/main.js
@@ -7,12 +7,28 @@ function createGridElement(dimensions) {
     grid.appendChild(row);
     for (var x = 0; x < dimensions; x++) {
       const cell = document.createElement("div");
+      cell.addEventListener("mouseenter", () => colourCell(cell));
       cell.classList.add("cell");
       row.appendChild(cell);
     }
   }
 
   return grid;
+}
+
+let mouseDown = false;
+function toggleMousePressed(state) {
+  mouseDown = state;
+}
+
+document.addEventListener("mousedown", () => toggleMousePressed(true));
+document.addEventListener("mouseup", () => toggleMousePressed(false));
+
+function colourCell(cell) {
+  if (!mouseDown) {
+    return;
+  }
+  cell.style.backgroundColor = "black";
 }
 
 const etchASketchBoard = document.querySelector(".etch-a-sketch-board");
